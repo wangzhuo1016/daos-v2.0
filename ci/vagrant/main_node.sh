b88c2009a617935ba713e6d35d628c31f5c1184a
Brian J. Murrell <brian.murrell@intel.com>
2021-10-29 07:45:40 -0400

    Fix nvme ftest arg
    Skip-PR-comments: true
    Skip-checkpatch: true
    Quick-build: true
    Skip-unit-tests: true
    Skip-func-test: true
    Skip-python-bandit: true
    Skip-coverity-test: true
    Skip-test-rpms: true
    Skip-vagrant-test: false
    RPM-test-version: 2.1.100-4.7348.g5dd4e939
    PR-repos: daos@PR-5498:160
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/vagrant/main_node.sh
+++ ci/vagrant/main_node.sh
@@ -41,7 +41,7 @@ case "$1" in
         REMOTE_ACCT="${REMOTE_ACCT:-vagrant}" \
         STAGE_NAME="$STAGE_NAME"              \
         TEST_TAG="$TEST_TAG:-basic"           \
-        FTEST_ARG=auto                        \
+        FTEST_ARG=--nvme:auto                 \
         PRAGMA_SUFFIX=-vm                     \
         NODE_COUNT="$NODE_COUNT"              \
         OPERATIONS_EMAIL="$OPERATIONS_EMAIL"  \
