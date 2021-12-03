d6ab916be391782c3cf1bc48bebe05a0f7eedbc0
craig <craig.durfey@intel.com>
2021-12-03 09:25:30 -0800

    Deployed 6fac564d8 to v1.2 with MkDocs 1.2.2 and mike 1.1.2

---
+++ v1.2/html/daos__uns_8h.js
@@ -0,0 +1,15 @@
+var daos__uns_8h =
+[
+    [ "DUNS_MAX_XATTR_LEN", "group___u_n_s.html#ga5358fccff57eb95f1cce94c2ad65becf", null ],
+    [ "DUNS_XATTR_FMT", "group___u_n_s.html#ga958a18f280f87a719addfe31f31aeaf2", null ],
+    [ "DUNS_XATTR_NAME", "group___u_n_s.html#gaae9d29885dfb321968f20d86baff6e48", null ],
+    [ "DUNS_NO_PREFIX", "group___u_n_s.html#gga94798fdadfbf49a7c658ace669a1d310ab39b23f27e62622c13de72d4d3eecb01", null ],
+    [ "DUNS_NO_REVERSE_LOOKUP", "group___u_n_s.html#gga94798fdadfbf49a7c658ace669a1d310af321a3c09a2d63f0f85ac9c5898766d1", null ],
+    [ "DUNS_NO_CHECK_PATH", "group___u_n_s.html#gga94798fdadfbf49a7c658ace669a1d310a3f61cd4e0a02927aa3a9ee16604fd6aa", null ],
+    [ "duns_create_path", "group___u_n_s.html#gaed5d956858070fac62f6effe9c6540e1", null ],
+    [ "duns_destroy_attr", "group___u_n_s.html#ga48aea4ccb3ac85b26f186daa47d98654", null ],
+    [ "duns_destroy_path", "group___u_n_s.html#ga5cd91a183d4b7ccf5d948739dff143c4", null ],
+    [ "duns_parse_attr", "group___u_n_s.html#ga678a3b99eceb1403bae24b874e45d8fd", null ],
+    [ "duns_resolve_path", "group___u_n_s.html#gabdbf8f5eb7ab131249f3675fc1b3c653", null ],
+    [ "duns_set_sys_name", "group___u_n_s.html#ga20cba354b6b1d19ed5ba3e36530e293d", null ]
+];
---
+++ v1.2/html/globals_dup.js
@@ -0,0 +1,6 @@
+var globals_dup =
+[
+    [ "d", "globals.html", null ],
+    [ "h", "globals_h.html", null ],
+    [ "n", "globals_n.html", null ]
+];
