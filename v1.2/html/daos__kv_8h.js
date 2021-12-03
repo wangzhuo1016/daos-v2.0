b3eaa1cf6c0c033c20c1931c68d08e5714b47202
craig <craig.durfey@intel.com>
2021-12-03 09:37:52 -0800

    Deployed 6fac564d8 to v1.2 with MkDocs 1.2.2 and mike 1.1.2

---
+++ v1.2/html/daos__kv_8h.js
@@ -0,0 +1,14 @@
+var daos__kv_8h =
+[
+    [ "DAOS_COND_KEY_GET", "group___k_v.html#gacb714e87cf7b9fcea1f294c0750bdb26", null ],
+    [ "DAOS_COND_KEY_INSERT", "group___k_v.html#ga0c3739abb79f2f6d630010c72b1769ca", null ],
+    [ "DAOS_COND_KEY_REMOVE", "group___k_v.html#ga80ab508926fb5ab4c10f8aca275316a6", null ],
+    [ "DAOS_COND_KEY_UPDATE", "group___k_v.html#ga28790aa071e2f975324089c87b077d94", null ],
+    [ "daos_kv_close", "group___k_v.html#gaf0bebe7eb7bf3c618b4fb71e8a39c71d", null ],
+    [ "daos_kv_destroy", "group___k_v.html#gada5a9ad798e7b54fcf6b8bbe674bbf36", null ],
+    [ "daos_kv_get", "group___k_v.html#ga23cae3d831736069a32833f64f5f5cc7", null ],
+    [ "daos_kv_list", "group___k_v.html#ga6fe2aa213fc2aab3840f20436cc9a6ae", null ],
+    [ "daos_kv_open", "group___k_v.html#gad05d00c41c670cd91e2e3fc8ca69f7bd", null ],
+    [ "daos_kv_put", "group___k_v.html#ga3f3cd19cee87b92e19b860b42458f848", null ],
+    [ "daos_kv_remove", "group___k_v.html#gafc6948a98b48daa2129054f4c47e55b1", null ]
+];
